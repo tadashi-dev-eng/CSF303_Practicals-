84324365bd062f0a9d199c7998efbac575bae974
tadashi-dev-eng <02230306.cst@rub.edu.bt>
2026-03-04 12:11:41 +0600

    wip

---
+++ cpp-practical-0/personal_intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string fullName = "Tashi Penjor"; 
+    int studentNumber = 02230306; 
+    string course = "BE Software Engineering";
+
+    cout << "Name: " << fullName <<endl;
+    cout << "Student No: " << studentNumber <<endl;
+    cout << "Course: " << course <<endl;
+
+    return 0;
+}
